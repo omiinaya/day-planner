e91f798acbd9b3dcca5a1d3e986f3b377ba6dd4b
Omar Minaya <omiinaya@gmail.com>
2020-01-31 03:46:52 -0800

    added logic to save data on button press.

---
+++ assets/scripts/script.js
@@ -0,0 +1,22 @@
+function loadData() {
+    document.getElementById("nine").value = localStorage.getItem("nine");
+    document.getElementById("ten").value = localStorage.getItem("ten");
+    document.getElementById("eleven").value = localStorage.getItem("eleven");
+    document.getElementById("twelve").value = localStorage.getItem("twelve");
+    document.getElementById("one").value = localStorage.getItem("one");
+    document.getElementById("two").value = localStorage.getItem("two");
+    document.getElementById("three").value = localStorage.getItem("three");
+    document.getElementById("four").value = localStorage.getItem("four");
+    document.getElementById("five").value = localStorage.getItem("five");
+}
+function saveData() {
+    localStorage.setItem("nine", $('#nine').val());
+    localStorage.setItem("ten", $('#ten').val());
+    localStorage.setItem("eleven", $('#eleven').val());
+    localStorage.setItem("twelve", $('#twelve').val());
+    localStorage.setItem("one", $('#one').val());
+    localStorage.setItem("two", $('#two').val());
+    localStorage.setItem("three", $('#three').val());
+    localStorage.setItem("four", $('#four').val());
+    localStorage.setItem("five", $('#five').val());
+}
